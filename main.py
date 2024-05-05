d60747e92066d520e427c899e1a878f487edf972
alireza mirhashemi <alimirhashemi78@gmail.com>
2024-05-05 11:26:32 +0330

    remove method fixed

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ class User:
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, User):
-            return self.username == other.username
+            return self.username == other.username and self.password == other.password
         elif isinstance(other, str):
             return self.username == other
+        # elif isinstance(other, )
+    
 
 
     def print_command_list():
@@ -29,7 +31,8 @@ class User:
         help
         exit
         """
-        print(commands)
+        for i, command in enumerate(commands.strip().split("\n")):
+            print(f"{i+1}. {command.strip()}")
 
     @staticmethod
     def is_valid(string: str):
@@ -48,9 +51,11 @@ class User:
         if User.is_valid(username) == False:
             print("username format is invalid")
             return
+
         if User.is_valid(password) == False:
             print("password format is invalid")
             return
+
         if User.is_username_exist(username):
             print("a user exists with this username")
             return
@@ -68,6 +73,16 @@ class User:
         if User.is_valid(password) == False:
             print("password format is invalid")
             return
+        
+        u = User(username, password)
+        if u in User.users:
+            User.users.remove(u)
+            print(f"removed [{username}] successfully")
+        elif u.username not in User.users:
+            print("no user exists with this username")
+        elif u.username in User.users and password not in User.users:
+            print("incorrect password")
+
 
         
 class Chess:
@@ -161,19 +176,23 @@ class King(Piece):
 while True:
     user_inp = input("").strip().split()
 
-    if user_inp == "exit":
+    if user_inp[0] not in ["register", "login", "remove", "list_users", "help", "exit"]:
+        print("invalid command")
+
+    if user_inp[0] == "register":
+        User.add_user(user_inp[1], user_inp[2])
+
+    if user_inp[0] == "remove":
+        User.remove_user(user_inp[1], user_inp[2])
+
+    if user_inp[0] == "exit":
         break
 
     if user_inp[0] == "help":
         User.print_command_list()
 
-    if len(user_inp) != 3:
-        print("invalid command")
+    if user_inp[0] in ["register", "login", "remove"] and len(user_inp) != 3:
+        print("invalid length of commands")
         continue
-
-    if user_inp[0] == "register":
-        User.add_user(user_inp[1], user_inp[2])
-    else:
-        print(f"invalid command <{user_inp[0]}>")
 
 
